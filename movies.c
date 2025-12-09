667edc7f697f60b47dc5bad570630cb01d7a7c95
TorneiraCromada <hideakifellipe@gmail.com>
2025-12-09 11:12:42 +0000

    aaa

---
+++ movies.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "movies.h"
+#include <string.h>
+
+void listallmovies(){}
+
+/*n sei se esse seria o tipo certo, to com sono e sepois eu penso nisso*/
+movies searchmovie(){}
+
+
+void showmovie(int code){}
+
+
+void addmovie(char title[], genres genres, char description[501], char director[25], /*lista, */int year, int duration, int rating, float revenue){}
+
+
+
